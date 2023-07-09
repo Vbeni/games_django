efdde2dea8048737c4bf4e1eb463c71eb0feb329
Vbeni <benivillafana@gmail.com>
2023-07-09 17:08:54 -0400

    games seed data

--- main_app/models.py
+++ main_app/models.py
@@ -12,3 +12,16 @@ class Game(models.Model):
     
     class Meta:
         ordering = ['name']
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.URLField(max_length=1024)
+    bio = models.TextField()
+    games = models.ManyToManyField(Game, related_name='characters')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
